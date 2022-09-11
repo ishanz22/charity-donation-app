b63d8379532af8c067cb3eb86c90d5cc04f71d3f
ishanz22 <ishanzloro22@gmail.com>
2022-09-12 04:08:49 +0530

    retrieve amount

--- src/components/RadioButtons.js
+++ src/components/RadioButtons.js
@@ -1,74 +1,45 @@
 import {View, Text, StyleSheet} from 'react-native';
-import React, {useEffect} from 'react';
+import React, { useState,useEffect } from 'react';
+
 import {RadioButton} from 'react-native-paper';
 import axios from 'axios';
 
 const RadioButtons = () => {
+    const [amountData, setamountData] = useState([])
     useEffect(() => {
         axios
             .get(
                 'https://07fplmn2nd.execute-api.us-west-2.amazonaws.com/dev/amounts',
                 {
                     headers: {
-                        'x-api-key': 'RHtTMSSIsm4ecSAfaHq4N7HpaMOJBv5utDDhp1ch',
+                        "x-api-key": "RHtTMSSIsm4ecSAfaHq4N7HpaMOJBv5utDDhp1ch",
                     },
-                },
+                }
             )
-            .then(response => {
-                console.log(response);
+            .then((res)=>{
+                setamountData(res.data.data)
             })
 
             .catch(function (error) {
                 console.log(error);
             });
-    }, []);
+    }, [])
 
-    const [checked, setChecked] = React.useState('first');
+    const [checked, setChecked] = React.useState(10);
     return (
+
         <View>
-            <View style={{flexDirection: 'row'}}>
-                <RadioButton
-                    value="first"
-                    status={checked === 'first' ? 'checked' : 'unchecked'}
-                    onPress={() => setChecked('first')}
-                />
-                <Text style={styles.amount}> $10</Text>
-            </View>
-
-            <View style={{flexDirection: 'row'}}>
-                <RadioButton
-                    value="second"
-                    status={checked === 'second' ? 'checked' : 'unchecked'}
-                    onPress={() => setChecked('second')}
-                />
-                <Text style={styles.amount}> $50</Text>
-            </View>
-            <View style={{flexDirection: 'row'}}>
-                <RadioButton
-                    value="second"
-                    status={checked === 'third' ? 'checked' : 'unchecked'}
-                    onPress={() => setChecked('third')}
-                />
-                <Text style={styles.amount}> $100</Text>
-            </View>
-            <View style={{flexDirection: 'row'}}>
-                <RadioButton
-                    value="fourth"
-                    status={checked === 'fourth' ? 'checked' : 'unchecked'}
-                    onPress={() => setChecked('fourth')}
-                />
-                <Text style={styles.amount}> $500</Text>
-            </View>
-
-            <View style={{flexDirection: 'row'}}>
-                <RadioButton
-                    value="fifth"
-                    status={checked === 'fifth' ? 'checked' : 'unchecked'}
-                    onPress={() => setChecked('fifth')}
-                />
-                <Text style={styles.amount}> $1000</Text>
-            </View>
-            <Text> </Text>
+            {amountData.map((el,i)=>
+                <View key={i} style={{flexDirection: 'row'}}>
+                    <RadioButton
+                        value={el}
+                        status={checked == el ? 'checked' : 'unchecked'}
+                        onPress={() => setChecked(el)}
+                    />
+                    <Text style={styles.amount}> ${el}</Text>
+                </View>
+            )}
+            <Text>{' '}</Text>
         </View>
     );
 };
