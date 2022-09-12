862003ba874d4d24a608e66deed2b18ae2a679f2
ishanz22 <ishanzloro22@gmail.com>
2022-09-12 16:03:58 +0530

    refactor

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
-import React, {Component, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './src/configurations/router';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from "./src/redux/store/ConfigureStore";
 import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
@@ -13,16 +10,15 @@ const App = () => {
             SplashScreen.hide();
         },2000)
     },[])
- 
+
 
     return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <NavigationContainer>
-                    <Router />
-                </NavigationContainer>
-            </PersistGate>
-        </Provider>
+
+        <NavigationContainer>
+            <Router />
+        </NavigationContainer>
+
+
     )
 }
 
